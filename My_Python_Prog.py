6d0042b767b039d143f3dc30610da5a7dd47471d
lotanganeap <136188067+lotanganeap@users.noreply.github.com>
2023-06-10 21:06:01 -0400

    Add files via upload

---
+++ My_Python_Prog.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Try your first Python output
+
+print('Hello, Python!')
+
+
+# In[2]:
+
+
+# Check the Python Version
+
+import sys
+print(sys.version)
+
+
+# In[3]:
+
+
+# Practice on writing comments
+
+print('Hello, Python!') # This line prints a string
+# print('Hi')
+
+
+# In[4]:
+
+
+# Try to see built-in error message
+
+print("Hello, Python!)
+
+
+# In[5]:
+
+
+print("This will be printed")
+frint("This will cause an error")
+print("This will NOT be printed")
+
+
+# In[6]:
+
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+print("Hello, world!")
+
+
+# In[7]:
+
+
+# Type of 12
+
+type(12)
+
+
+# In[8]:
+
+
+# Type of 2.14
+
+type(2.14)
+
+
+# In[9]:
+
+
+# Type of "Hello, Python 101!"
+
+type("Hello, Python 101!")
+
+
+# In[10]:
+
+
+type(12.0)
+
+
+# In[11]:
+
+
+# Print the type of -1
+
+type(-1)
+
+
+# In[12]:
+
+
+# Print the type of 4
+
+type(4)
+
+
+# In[13]:
+
+
+# Print the type of 0
+
+type(0)
+
+
+# In[14]:
+
+
+# Print the type of 1.0
+
+type(1.0) # Notice that 1 is an int, and 1.0 is a float
+
+
+# In[15]:
+
+
+# System settings about float type
+
+sys.float_info
+
+
+# In[16]:
+
+
+float(2)
+
+
+# In[17]:
+
+
+# Convert integer 2 to a float and check its type
+
+type(float(2))
+
+
+# In[18]:
+
+
+# Casting 1.1 to integer will result in loss of information
+
+int(1.1)
+
+
+# In[19]:
+
+
+# Convert a string into an integer
+
+int('1')
+
+
+# In[20]:
+
+
+int('1 or 2 people')
+
+
+# In[21]:
+
+
+float('1.2')
+
+
+# In[22]:
+
+
+str(1)
+
+
+# In[23]:
+
+
+# Value true
+
+True
+
+
+# In[24]:
+
+
+type(True)
+
+
+# In[25]:
+
+
+int(True)
+
+
+# In[26]:
+
+
+bool(1)
+
+
+# In[27]:
+
+
+float(True)
+
+
+# In[28]:
+
+
+type(6/2)
+
+
+# In[29]:
+
+
+type(6//2)
+
+
+# In[30]:
+
+
+# Addition operation expression
+
+43 + 60 + 16 + 41
+
+
+# In[31]:
+
+
+160/60
+
+
+# In[32]:
+
+
+# Store value into variable
+
+x = 43 + 60 + 16 + 41
+
+
+# In[33]:
+
+
+x
+
+
+# In[34]:
+
+
+y=x/60
+
+
+# In[35]:
+
+
+y
+
+
+# In[36]:
+
+
+x = 3 + 2 * 2
+
+
+# In[37]:
+
+
+x
+
+
+# In[ ]:
+
+
+
+
